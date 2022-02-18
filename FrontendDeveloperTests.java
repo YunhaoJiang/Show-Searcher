a1b534944a5023b6d8bc9803fa112fc6edc87a38
Yunhao Jiang <jiang297@wisc.edu>
2022-02-17 19:39:11 -0600

    add frontend class and tests (empty)

---
+++ FrontendDeveloperTests.java
@@ -0,0 +1,66 @@
+public class FrontendDeveloperTests {
+  public static boolean test1() {
+    try {
+      System.out.println("");
+    } catch (Exception e) {
+      System.out.println(new Throwable().getStackTrace()[0].getMethodName() + " failed: an "
+          + "unexpected exception has been thrown.");
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+  public static boolean test2() {
+    try {
+      System.out.println("");
+    } catch (Exception e) {
+      System.out.println(new Throwable().getStackTrace()[0].getMethodName() + " failed: an "
+          + "unexpected exception has been thrown.");
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+  public static boolean test3() {
+    try {
+      System.out.println("");
+    } catch (Exception e) {
+      System.out.println(new Throwable().getStackTrace()[0].getMethodName() + " failed: an "
+          + "unexpected exception has been thrown.");
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+  public static boolean test4() {
+    try {
+      System.out.println("");
+    } catch (Exception e) {
+      System.out.println(new Throwable().getStackTrace()[0].getMethodName() + " failed: an "
+          + "unexpected exception has been thrown.");
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+  public static boolean test5() {
+    try {
+      System.out.println("");
+    } catch (Exception e) {
+      System.out.println(new Throwable().getStackTrace()[0].getMethodName() + " failed: an "
+          + "unexpected exception has been thrown.");
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+
+  public static void main (String[] args){
+    System.out.println("test1: " + test1());
+    System.out.println("test2: " + test2());
+    System.out.println("test3: " + test3());
+    System.out.println("test4: " + test4());
+    System.out.println("test5: " + test5());
+  }
+
+}
---
+++ ShowSearcherFrontend.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class ShowSearcherFrontend implements IShowSearcherFrontend {
+  /**
+   * This method drives the entire read, eval, print loop (repl) for the
+   * Song Search App.  This loop will continue to run until the user
+   * explicitly enters the quit command.
+   */
+  @Override
+  public void runCommandLoop() {
+
+  }
+
+  @Override
+  public void displayCommandMenu() {
+
+  }
+
+  @Override
+  public void displayShows(List<IShow> shows) {
+
+  }
+
+  @Override
+  public void titleSearch() {
+
+  }
+
+  @Override
+  public void yearSearch() {
+
+  }
+}
