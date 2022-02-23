f30004e361a6f66e690c75ed64eb1dd1a6671021
Mojtaba Javid <javid2@wics.edu>
2022-02-23 18:07:15 +0000

    initial raw code

---
+++ Show.java
@@ -0,0 +1,34 @@
+
+public class Show implements IShow{
+
+	@Override
+	public int compareTo(IShow o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getYear() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRating() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isAvailableOn(String provider) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ ShowLoader.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class ShowLoader implements IShowLoader{
+
+	@Override
+	public List<IShow> loadShows(String filepath) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
